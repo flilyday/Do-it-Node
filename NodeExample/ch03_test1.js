6b5dda59a8e46cef47ec6a17aef5e8897c04e965
lemon <eunbiryung@gmail.com>
2020-12-06 20:44:30 +0900

    JS기초 : 변수, 객체

---
+++ NodeExample/ch03_test1.js
@@ -0,0 +1,9 @@
+var name;
+
+var age = 20;
+
+console.log(name);
+console.log(age);
+
+var name = 'sosi';
+console.log('name : ' + name);
---
+++ NodeExample/ch03_test2.js
@@ -0,0 +1,7 @@
+var person = {}
+
+person['name'] = 'sosi'
+person['age'] = 20;
+
+console.log('이름 :' + person.name)
+console.log('나이 : ' + person['age'])
