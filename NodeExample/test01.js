c1cc787fffb49eff5bcbbbed31043cda609579d3
lemon <eunbiryung@gmail.com>
2020-12-06 14:14:30 +0900

    대표적인 전역객체인 console과 process

--- NodeExample/test01.js
+++ NodeExample/test01.js
@@ -1,5 +1,4 @@
 console.log("Hello")
-
 console.log("숫자 : %d", 10)
 console.log("문자 : %s", 'character')
 
@@ -7,5 +6,21 @@ var sosi = {
     name : '소녀시대',
     age : '20'
 }
-
 console.log("자바스크립트객체 : %j", sosi)
+
+//자주 사용하는 console객체의 매서드
+//1.해당 객체가 가지는 모든 값을 출력 console.dir
+console.dir(sosi)
+
+//2.시간을 측정 console.time
+console.time('duration')
+var result = 0;
+for (i=0; i<10000; i++) {
+    result += i;
+}
+console.timeEnd('duration')
+
+//3.전역변수 __filename과 __dirname
+
+console.log("파일 이름 : %s", __filename);
+console.log("파일 패스 : %s", __dirname);
