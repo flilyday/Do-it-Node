1bd78175779e517b1430b6a2457b2a1d6ac064ff
lemon <eunbiryung@gmail.com>
2020-12-07 22:07:18 +0900

    배열 매서드 정리

---
+++ NodeExample/ch03_test14-1.js
@@ -0,0 +1,11 @@
+배열의 매서드 정리
+
+users = []
+
+users.push() : 뒷쪽에 하나 넣기
+users.pop() : 뒷쪽에 하나 삭제하기
+users.unshift() : 앞쪽에 하나 넣기
+users.shift() : 앞쪽에 하나 빼기
+users.splice(n번째, 뺄 갯수) : n번째 중간에 하나 빼기
+users.splice(n번째, 0, 넣을 내용) : n번째 중간에 넣기(빼는게 0이니까)
+users.slice(n번째, m) : n번째부터 m-1개까지 빼기
